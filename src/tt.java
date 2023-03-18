fa65d0d391a130bab4e3616db63e2560d72c5671
kaolacokie <127673932+kaolacokie@users.noreply.github.com>
2023-03-18 19:35:03 +0800

    pull commit

--- src/tt.java
+++ src/tt.java
@@ -2,7 +2,7 @@ public class tt {
     public static void main(String args[]){
         System.out.println("bbb");
         System.out.println("bbb");
-        System.out.println("bbb");
+        
 
     }
 }
